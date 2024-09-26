0399a55e2a8cc2a54377e6664669b1caf3f84122
owenga <owengajoseph54@gmail.com>
2024-09-26 17:49:39 +0300

    commits

---
+++ array_overloading/main.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+
+#include <iostream>
+
+IntArray fillArray()
+{
+	IntArray a(5);
+
+	a[0] = 5;
+	a[1] = 8;
+	a[2] = 2;
+	a[3] = 3;
+	a[4] = 6;
+
+	return a;
+}
+
+int main()
+{
+	IntArray a{ fillArray() };
+
+	std::cout << a << '\n';
+
+	auto& ref{ a }; // we're using this reference to avoid compiler self-assignment errors
+	a = ref;
+
+	IntArray b(1);
+	b = a;
+
+	a[4] = 7;
+
+	std::cout << b << '\n';
+
+	return 0;
+}
